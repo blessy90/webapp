2e49784b9ec53643d52e69c5c1a89784b403966d
Blessy John <blessy.john@ad.infosys.com>
2017-05-18 15:02:24 +0530

    Test cases for Hello World.

--- assignment/webapp/tests.py
+++ assignment/webapp/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
-
+from webapp import views
 # Create your tests here.
+
+"""To test the webapp which displays the Hello World."""
+class HelloWorldTestCase(TestCase):
+    def test_request_status_hello_world(self):
+        request = 'fake request'
+        response = views.index(request)
+        self.assertEqual(response.status_code,200)
+        self.assertContains(response,"Hello World.",status_code=200)
